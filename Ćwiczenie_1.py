886aa7d1a9d9221f1f113a71c4d0529dd623e0cf
unknown <weronika_kucharczyk@outlook.com>
2026-04-28 15:22:48 +0200

    replace lambda with def function and add docstring

--- Ćwiczenie_1.py
+++ Ćwiczenie_1.py
@@ -1,3 +1,12 @@
-slowa= ["kajak", "butelka", "długopis", "anna", "woda"]
-sprawdzenie = list(map(lambda s: s == s[::-1], slowa))
-print(sprawdzenie)
+def czy_palindrom(slowo):
+    """
+    Sprawdza, czy podany wyraz jest palindromem.
+    Argument: slowo (string)
+    Zwraca: boolean (True/False)
+    """
+    return slowo == slowo[::-1]
+
+slowa = ["kajak", "butelka", "długopis", "anna", "woda"]
+wyniki = [czy_palindrom(s) for s in slowa]
+
+print(wyniki)
